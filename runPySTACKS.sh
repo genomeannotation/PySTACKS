1f45985873f285943b0fab1ab3d1b4e899dc3e67
Lars Bergstrom <lbergstrom@gmail.com>
2016-07-19 14:01:15 -1000

    generic execution sample

--- runPySTACKS.sh
+++ runPySTACKS.sh
@@ -3,4 +3,4 @@ export PATH=$PATH:/data0/opt/AlignmentSoftware/bwa/bwa-0.7.12/
 module load samtools
 
 ##execute PySTACKS
-python pystacks.py -sp HwnDros -d raw_datas.txt -r /projects/geibLabs/OutsideProjects/HawaiianDrosophila/Dhet_ddRAD/ReferenceGenome/DHET_01v.201501.genome.fa -i index_files.txt -s 1 -pm All_HwnDros_one_pop.txt -np 2
+python pystacks.py -sp name -d raw_datas.txt -r genome_data.fa -i index_files.txt -s 1 -pm species_pop.txt -np 2

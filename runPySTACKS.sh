77bccf5c5527dff773dc3e9f43a9ef9283f1348d
Lars Bergstrom <lbergstrom@gmail.com>
2016-07-20 09:33:10 -1000

    load python module

--- runPySTACKS.sh
+++ runPySTACKS.sh
@@ -1,6 +1,7 @@
 module load stacks
 export PATH=$PATH:/data0/opt/AlignmentSoftware/bwa/bwa-0.7.12/
 module load samtools
+module load python/3.4.1
 
 ##execute PySTACKS
 python pystacks.py -sp name -d raw_datas.txt -r genome_data.fa -i index_files.txt -s 1 -pm species_pop.txt -np 2
